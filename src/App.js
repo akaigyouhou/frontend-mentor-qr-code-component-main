eff15f6c54b19236276ed49f4ae7b94ab869c1ad
EdwardLi <akaigyouhou@gmail.com>
2022-05-22 18:35:04 +0800

    adjust html landmark

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
         <Card />
         {/* <div className="attribution">
           Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-          Coded by <a href="#">Your Name Here</a>.
+          Coded by <a href="#">akaigyouhou</a>.
         </div> */}
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import QRCode from './image-qr-code.png';
 
 function Card() {
     return (
-        <div id="card">
+        <main id="card">
             <div className="content">
                 <img src={QRCode} alt="qrcode"></img>
                 <div className="desp">
@@ -10,7 +10,7 @@ function Card() {
                     <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
                 </div>
             </div>
-        </div>
+        </main>
     )
 }
 
